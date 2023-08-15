966c4b1c6e646a3f5a645053be8a1f78a4023da2
Hadija <kerimbaevahadija@gmail.com>
2023-08-15 16:30:14 +0600

    finished styled carts

--- src/components/PhoneCarts.jsx
+++ src/components/PhoneCarts.jsx
@@ -5,11 +5,25 @@ export const PhoneCarts = ({phone}) => {
   return (
     <StyledPhoneCart>
       <div key={phone.id}>
-        <img src={phone.image} alt="iphone" />
+        <StyledImage src={phone.image} alt="iphone" />
         <h3>{phone.title}</h3>
         <h3>{phone.price}</h3>
       </div>
     </StyledPhoneCart>
   );
 };
-const StyledPhoneCart = styled('div')
+const StyledImage = styled('img')`
+    width: 370px;
+    height: 500px;
+`
+const StyledPhoneCart = styled('div')`
+    box-sizing: border-box;
+    overflow: hidden;
+    width: 400px;
+    background-color: white;
+    padding: 15px;
+    margin-top: 30px;
+    margin-left: 30px;
+    margin-bottom: 30px;
+    border-radius: 15px;
+`
